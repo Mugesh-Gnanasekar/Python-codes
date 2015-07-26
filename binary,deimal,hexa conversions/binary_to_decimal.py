263d8c83070611efc56dd3e1f96f130b402fe372
mugeshsg <mshanmug@mtu.edu>
2015-07-25 23:05:20 -0400

    more conversion

--- binary,deimal,hexa conversions/binary_to_decimal.py
+++ binary,deimal,hexa conversions/binary_to_decimal.py
@@ -1,9 +1,9 @@
 '''
 In this program, we will convert a binary number (base 2) to a decimal number
-(base 10)
+(base 10). We will do in two different ways and the latter being very simple!
 '''
 
-def B2D(bin_num):
+def B2D_1(bin_num):
     bin_list=bin_num.strip()
     bin_int=[]
     for value in bin_list:
@@ -18,6 +18,20 @@ def B2D(bin_num):
         i+=1
     return decimal_value
 
+def B2D_2(bin_num):
+    bin_num_int=int(bin_num)
+    decimal_value=0
+    power=0
+    while bin_num_int>0:
+        decimal_value += (bin_num_int%10)*2**power
+        bin_num_int//=10
+        power+=1
+    return decimal_value
+
+
+
 bin_num=input('Enter the binary number that needs to be converted to decimal:')
-answer=B2D(bin_num)
-print (answer)
+answer_1=B2D_1(bin_num)
+print (answer_1)
+answer_2=B2D_2(bin_num)
+print (answer_2)
